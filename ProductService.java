2a8dbc755a2dbab695930fd2c2b7e29f3cf5a629
anboy0 <angelaboy9@gmail.com>
2018-01-18 17:05:15 +0800

    第一次添加了一个Java文件

---
+++ ProductService.java
@@ -0,0 +1,11 @@
+package cn.itcast.service;
+
+import cn.itcast.pojo.ResultModel;
+
+public interface ProductService {
+	
+	// 查询商品一览
+	public ResultModel findProductResults(String queryString, String catalog_name, 
+			String price, Integer page, String sort) throws Exception;
+
+}
