41d23263ec30bc9e43892370d42e91fea136cdb5
anboy0 <angelaboy9@gmail.com>
2018-01-18 17:09:11 +0800

    添加了变量 a

--- ProductService.java
+++ ProductService.java
@@ -1,4 +1,4 @@
-package cn.itcast.service;
+﻿package cn.itcast.service;
 
 import cn.itcast.pojo.ResultModel;
 
@@ -7,5 +7,7 @@ public interface ProductService {
 	// 查询商品一览
 	public ResultModel findProductResults(String queryString, String catalog_name, 
 			String price, Integer page, String sort) throws Exception;
+	
+	private int a;
 
 }
